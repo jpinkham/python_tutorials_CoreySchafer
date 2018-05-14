73a5b1d1f726ce2dc1d806d45ac87219fcf2633c
Jennifer Pinkham <jpinkham@cpan.org>
2018-05-14 11:28:12 -0400

    Simple module used for testing imports

---
+++ mymodules/jp_module.py
@@ -0,0 +1,12 @@
+print('Imported jp_module...')
+
+test = 'Tester String'
+
+
+def find_index(to_search, target):
+    '''Find the index of a value in a sequence'''
+    for i, value in enumerate(to_search):
+        if value == target:
+            return i
+
+    return -1
